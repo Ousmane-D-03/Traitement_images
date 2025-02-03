3fdb60fe3deaed7cadc4bd0c6c16e529a112d180
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-02-03 21:29:20 +0100

    augementer ou diminuer la luminosite

--- tp-image/src/main/java/imageprocessing/GrayLevelProcessing.java
+++ tp-image/src/main/java/imageprocessing/GrayLevelProcessing.java
@@ -20,7 +20,16 @@ public class GrayLevelProcessing {
 		}
 	}
 
-    
+    public static void greyChange(int delta,GrayU8 input){
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int gl = input.get(x, y)+delta;
+				if (gl > 255) gl = 255;
+				else if(gl<0) gl =0;
+				input.set(x, y, gl);
+			}
+		}
+	}
 
     public static void main( String[] args ) {
 
@@ -29,8 +38,9 @@ public class GrayLevelProcessing {
 			System.out.println("missing input or output image filename");
 			System.exit(-1);
 		}
-		final String inputPath = args[0];
-		GrayU8 input = UtilImageIO.loadImage(inputPath, GrayU8.class);
+		final String inputPath = args[1];
+		int delta = Integer.valueOf(args[0]);
+		GrayU8 input = boofcv.io.image.UtilImageIO.loadImage(inputPath, GrayU8.class);
 		if(input == null) {
 			System.err.println("Cannot read input file '" + inputPath);
 			System.exit(-1);
@@ -38,10 +48,11 @@ public class GrayLevelProcessing {
 
 		// processing
 		
-        threshold(input, 128);
+        //threshold(input, 128);
+		greyChange(delta, input);
 		
 		// save output image
-		final String outputPath = args[1];
+		final String outputPath = args[2];
 		UtilImageIO.saveImage(input, outputPath);
 		System.out.println("Image saved in: " + outputPath);
 	}
