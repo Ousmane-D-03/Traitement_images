eccc618d9ef2ecfd377dfbd1d9f83bcf9e6bae2d
Ousmane Sarr Diop <ousmane.diop.1@etu.u-bordeaux.fr>
2025-02-03 15:49:08 +0100

    initialisation

---
+++ tp-image/src/main/java/imageprocessing/GrayLevelProcessing.java
@@ -0,0 +1,49 @@
+package imageprocessing;
+
+import boofcv.io.image.UtilImageIO;
+import boofcv.struct.image.GrayU8;
+
+
+public class GrayLevelProcessing {
+
+	public static void threshold(GrayU8 input, int t) {
+		for (int y = 0; y < input.height; ++y) {
+			for (int x = 0; x < input.width; ++x) {
+				int gl = input.get(x, y);
+				if (gl < t) {
+					gl = 0;
+				} else {
+					gl = 255;
+				}
+				input.set(x, y, gl);
+			}
+		}
+	}
+
+    
+
+    public static void main( String[] args ) {
+
+    	// load image
+		if (args.length < 2) {
+			System.out.println("missing input or output image filename");
+			System.exit(-1);
+		}
+		final String inputPath = args[0];
+		GrayU8 input = UtilImageIO.loadImage(inputPath, GrayU8.class);
+		if(input == null) {
+			System.err.println("Cannot read input file '" + inputPath);
+			System.exit(-1);
+		}
+
+		// processing
+		
+        threshold(input, 128);
+		
+		// save output image
+		final String outputPath = args[1];
+		UtilImageIO.saveImage(input, outputPath);
+		System.out.println("Image saved in: " + outputPath);
+	}
+
+}
